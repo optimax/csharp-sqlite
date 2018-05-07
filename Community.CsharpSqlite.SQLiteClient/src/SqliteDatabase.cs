f9b0c539b7ce6b79266da7a6934de86053870638
AndreW <andrew@optimax.com>
2018-05-06 21:25:22 -0500

    Upgraded to .NET 4. Expanded SqliteDatabase with GetRecords, Insert, Update, ListTables, GetTable.

--- Community.CsharpSqlite.SQLiteClient/src/SqliteDatabase.cs
+++ Community.CsharpSqlite.SQLiteClient/src/SqliteDatabase.cs
@@ -1,4 +1,6 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
 using System.Data.Common;
 
 namespace Community.CsharpSqlite.SQLiteClient
@@ -10,10 +12,8 @@ namespace Community.CsharpSqlite.SQLiteClient
         public bool IsOpen => Connection?.State == ConnectionState.Open;
 
 
-
         private string ConnectionString { get; set; }
         private DbConnection Connection { get; set; }
-
 
 
         public SqliteDatabase()
@@ -83,6 +83,95 @@ namespace Community.CsharpSqlite.SQLiteClient
             cmd.CommandText = sql;
             return cmd.ExecuteScalar();
         }
+
+
+
+
+        public IEnumerable<Dictionary<string, object>> GetRecords(string sql)
+        {
+            var result = new List<Dictionary<string, object>>();
+            using (var reader = ExecuteReader(sql))
+            {
+                while (reader.Read())
+                {
+                    var fieldCount = reader.FieldCount;
+                    var rec = new Dictionary<string, object>();
+                    for (int fieldNo = 0; fieldNo < fieldCount; fieldNo++)
+                    {
+                        var fieldName = reader.GetName(fieldNo);
+                        var fieldValue = reader.GetValue(fieldNo);
+                        rec[fieldName] = fieldValue;
+                    }
+                    result.Add(rec);
+                }
+            }
+            return result;
+
+        }
+
+
+        public IEnumerable<string> ListTables()
+        {
+            var result = new List<string>();
+            string sql = "SELECT name FROM SQLITE_MASTER where type='table'";
+            using (var reader = ExecuteReader(sql))
+            {
+                while (reader.Read())
+                {
+                    var tableName = reader["Name"];
+                    result.Add(tableName.ToString());
+                }
+            }
+            return result;
+        }
+
+
+        /// <summary>
+        /// Return the results of a sql query as a table
+        /// </summary>
+        public DataTable GetTable(string sql)
+        {
+            DataTable dt = new DataTable();
+            using (var reader = ExecuteReader(sql))
+                dt.Load(reader);
+            return dt;
+        }
+
+
+
+
+
+        public void Insert(string tableName, Dictionary<string,object> rec)
+        {
+            if (rec.Count == 0)
+                throw new Exception("Record contains no data to insert.");
+            var columns = "";
+            var values = "";
+            foreach (var val in rec)
+            {
+                columns += $" {val.Key},";
+                values += $" '{val.Value}',";
+            }
+            columns = columns.Substring(0, columns.Length - 1);
+            values = values.Substring(0, values.Length - 1);
+            ExecuteCommand($"insert into {tableName} ({columns}) values ({values});");
+        }
+
+
+
+        public void Update(string tableName, Dictionary<string, object> rec, string whereClause)
+        {
+            if (rec.Count == 0)
+                throw new Exception("Record contains no data to update.");
+            if (string.IsNullOrWhiteSpace(whereClause))
+                throw new Exception("You must provide a 'where'-clause.");
+            var values = "";
+            foreach (var val in rec)
+                values += $" {val.Key} = '{val.Value}',";
+            values = values.Substring(0, values.Length - 1);
+            ExecuteCommand($"update {tableName} set {values} where {whereClause};");
+        }
+
 
     }
 }
